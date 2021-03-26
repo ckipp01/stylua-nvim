112b5a2414d660f783e8e3629df5b695a4b83fb3
ckipp01 <ckipp@pm.me>
2021-03-26 16:29:02 +0100

    Add format issue into loclist

--- lua/stylua-nvim.lua
+++ lua/stylua-nvim.lua
@@ -27,14 +27,35 @@ M.format_file = function()
 	local output = fn.system(stylua_command, buf_get_full_text(0))
 
 	if fn.empty(output) == 0 then
+		-- Clear llist of old issues from previous format
+		vim.fn.setloclist(0, {})
+		vim.cmd("lclose")
+
 		local new_lines = vim.fn.split(output, "\n")
+		-- TODO we should probably do a compare to what we have to not change the
+		-- buffer if we don't have to
 		api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
 	else
 		local errors = table.concat(vim.fn.readfile(error_file), " ")
-		-- TODO parse the errors better here. it looks like we actually get
-		-- a range in this string. We may be able to parse this out and put it
-		-- in the quickfix list
-		print(errors)
+
+		-- A little hacky, but we know that the error messsages are always shaped
+		-- in a similiar way containing:
+		--(starting from line 32, character 2 and ending on line 32, character 5)
+		-- So we know that:
+		--	- locations[1] = start line
+		--	- locations[2] = start col
+		--	- locations[3] = end line
+		--	- locations[4] = end col
+		local locations = {}
+		for num in errors:gmatch("%d+") do
+			table.insert(locations, num)
+		end
+
+		-- Ensure that we have the full range
+		if table.getn(locations) == 4 then
+			vim.fn.setloclist(0, { { bufnr = 0, lnum = locations[1], col = locations[2], text = errors } })
+			vim.cmd("lopen")
+		end
 	end
 
 	fn.delete(error_file)
